4624c2d5ea832d91870f7648877623a1e87dae25
yoo-jaein <y_jaein@naver.com>
2017-07-22 17:53:45 +0900

    First commit

---
+++ app/src/main/java/com/example/myapplication/Activity_intro.java
@@ -0,0 +1,41 @@
+package com.example.myapplication;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.Window;
+
+public class Activity_intro extends Activity {
+    private Handler handler;
+
+    Runnable runnable = new Runnable() {
+        @Override
+        public void run() {
+            Intent intent = new Intent(Activity_intro.this, MainActivity.class);
+            startActivity(intent);
+            finish();
+            overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out);
+        }
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_intro);
+        init();
+
+        handler.postDelayed(runnable, 1500);
+    }
+
+    public void init() {
+        handler = new Handler();
+    }
+
+    @Override
+    public void onBackPressed(){
+        super.onBackPressed();
+        handler.removeCallbacks(runnable);
+    }
+}
+
